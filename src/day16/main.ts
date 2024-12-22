846f5f6da964985bfb074899809dc77891d9faed
Gaute Meek Olsen <gaute.meek@gmail.com>
2024-12-22 21:27:20 +0100

    day16

--- src/day16/main.ts
+++ src/day16/main.ts
@@ -1,9 +1,9 @@
+import { isNeighbour } from '../common/point.ts'
 import { Point } from '../common/point.ts'
 
-// TODO - refactor
+type Node = Point & { dir: '^' | 'v' | '<' | '>' }
 
-type Reindeer = Point & { direction: '^' | '>' | 'v' | '<' }
-type Maze = { map: string[]; start: Reindeer; goal: Point }
+const cachedDijkstra = new Map<string, { dist: Map<string, number>; prev: Map<string, Node[]> }>()
 
 if (import.meta.main) {
   const input = await Deno.readTextFile('./inputs/day16.txt')
@@ -12,125 +12,120 @@ if (import.meta.main) {
 }
 
 export function part1(input: string) {
-  const { map, start } = getMap(input)
-  const visited = new Map<string, number>()
-  visited.set(stateAsKey(start), 0)
-  const toCheck = [{ on: start, score: 0 }]
-  let best = Infinity
-  while (true) {
-    if (toCheck.length === 0) break
-    const next = toCheck.sort((a, b) => b.score - a.score).pop()!
-    if (next.score > best) {
-      continue
-    }
-    visited.set(stateAsKey(next.on), next.score)
-    const actions = nextOptions(next.on)
-    for (const action of actions) {
-      const newScore = next.score + action.addedScore
-      if (map[action.reindeer.y][action.reindeer.x] === '#') {
-        continue
-      }
-      if (visited.has(stateAsKey(action.reindeer)) && visited.get(stateAsKey(action.reindeer))! < newScore) {
-        continue
-      }
-      if (toCheck.some((check) => stateAsKey(check.on) === stateAsKey(action.reindeer) && check.score < newScore)) {
-        continue
-      }
-      if (map[action.reindeer.y][action.reindeer.x] === 'E') {
-        best = Math.min(best, newScore)
-        continue
-      }
-      toCheck.push({ on: action.reindeer, score: newScore })
-    }
-  }
-  return best
+  const { start, goal, tiles } = getMap(input)
+  const { dist, prev } = dijkstra(tiles, start)
+  cachedDijkstra.set(input, { dist, prev })
+  return goal.map((g) => dist.get(key(g))).sort()[0]
 }
 
 export function part2(input: string) {
-  const { map, start } = getMap(input)
-  const visited = new Map<string, number>()
-  visited.set(stateAsKey(start), 0)
-  const toCheck = [{ on: start, score: 0, trail: [start] }]
-  let best = Infinity
-  const partOfBest = new Set<string>()
-  while (true) {
-    if (toCheck.length === 0) break
-    const next = toCheck.sort((a, b) => b.score - a.score).pop()!
-    if (next.score > best) {
-      continue
-    }
-    visited.set(stateAsKey(next.on), next.score)
-    const actions = nextOptions(next.on)
-    for (const action of actions) {
-      const newScore = next.score + action.addedScore
-      if (map[action.reindeer.y][action.reindeer.x] === '#') {
-        continue
+  const { start, goal, tiles } = getMap(input)
+  const { dist, prev } = cachedDijkstra.has(input) ? cachedDijkstra.get(input)! : dijkstra(tiles, start)
+  const best = goal.sort((a, b) => (dist.get(key(a)) ?? Infinity) - (dist.get(key(b)) ?? Infinity))[0]
+  const tilesPartOfBest = [best]
+  const nodeQueue = [best]
+  const checked = [key(best)]
+  while (nodeQueue.length > 0) {
+    const current = nodeQueue.shift()!
+    const prevs = prev.get(key(current))
+    if (prevs) {
+      for (const p of prevs) {
+        if (!checked.includes(key(p))) {
+          checked.push(key(p))
+          nodeQueue.push(p)
+          if (!tilesPartOfBest.some((t) => t.x === p.x && t.y === p.y)) {
+            tilesPartOfBest.push(p)
+          }
+        }
       }
-      if (visited.has(stateAsKey(action.reindeer)) && visited.get(stateAsKey(action.reindeer))! < newScore) {
-        continue
-      }
-      if (toCheck.some((check) => stateAsKey(check.on) === stateAsKey(action.reindeer) && check.score < newScore)) {
-        continue
-      }
-      if (map[action.reindeer.y][action.reindeer.x] === 'E') {
-        best = Math.min(best, newScore)
-        ;[...next.trail, action.reindeer].forEach((r) => partOfBest.add(`${r.x},${r.y}`))
-        continue
-      }
-      toCheck.push({ on: action.reindeer, score: newScore, trail: [...next.trail, action.reindeer] })
     }
   }
-  return partOfBest.size
+  return tilesPartOfBest.length
 }
 
-function getMap(input: string): Maze {
-  const map = input.split('\n')
-  const reindeerY = map.findIndex((line) => line.includes('S'))
-  const reindeerX = map[reindeerY].indexOf('S')
-  const goalY = map.findIndex((line) => line.includes('E'))
-  const goalX = map[goalY].indexOf('E')
-  return {
-    map,
-    start: { x: reindeerX, y: reindeerY, direction: '>' },
-    goal: { x: goalX, y: goalY },
+function getMap(input: string) {
+  let start: Node | null = null
+  const goal: Node[] = []
+  const tiles: Node[] = []
+  const lines = input.split('\n')
+  for (let y = 0; y < lines.length; y++) {
+    const line = lines[y]
+    for (let x = 0; x < line.length; x++) {
+      const canPointHorizontal = line[x - 1] === '.' || line[x + 1] === '.'
+      const canPointVertical = lines[y - 1]?.[x] === '.' || lines[y + 1]?.[x] === '.'
+      if (line[x] === 'S') {
+        start = { x, y, dir: '>' }
+        tiles.push(start)
+      } else if (line[x] === 'E') {
+        if (canPointHorizontal) {
+          goal.push({ x, y, dir: '>' })
+          goal.push({ x, y, dir: '<' })
+        }
+        if (canPointVertical) {
+          goal.push({ x, y, dir: '^' })
+          goal.push({ x, y, dir: 'v' })
+        }
+      } else if (line[x] === '.') {
+        if (canPointHorizontal) {
+          tiles.push({ x, y, dir: '>' })
+          tiles.push({ x, y, dir: '<' })
+        }
+        if (canPointVertical) {
+          tiles.push({ x, y, dir: '^' })
+          tiles.push({ x, y, dir: 'v' })
+        }
+      }
+    }
   }
+  return { start: start!, goal, tiles: [...tiles, ...goal] }
 }
 
-function stateAsKey(reindeer: Reindeer) {
-  return `${reindeer.x},${reindeer.y}:${reindeer.direction}`
+/*
+Modification: prev is array of node to support multiple best paths
+*/
+export function dijkstra(graphNodes: Node[], start: Node) {
+  const queue: Node[] = []
+  const dist = new Map<string, number>()
+  const prev = new Map<string, Node[]>()
+  graphNodes.forEach((node) => {
+    dist.set(key(node), Infinity)
+    queue.push(node)
+  })
+  dist.set(key(start), 0)
+
+  while (queue.length > 0) {
+    const u = queue.sort((a, b) => dist.get(key(a))! - dist.get(key(b))!).shift()!
+
+    const neighboursInQueue = queue.filter((p) => canGo(u, p))
+    neighboursInQueue.forEach((v) => {
+      const alt = dist.get(key(u))! + (u.dir === v.dir ? 1 : 1001)
+      if (alt <= dist.get(key(v))!) {
+        dist.set(key(v), alt)
+        if (prev.has(key(v))) {
+          prev.get(key(v))?.push(u)
+        } else {
+          prev.set(key(v), [u])
+        }
+      }
+    })
+  }
+  return { dist, prev }
 }
 
-function nextOptions(reindeer: Reindeer): { reindeer: Reindeer; addedScore: number }[] {
-  const turns: ('^' | '>' | 'v' | '<')[] = []
-  if (reindeer.direction === '^' || reindeer.direction === 'v') {
-    turns.push('>', '<')
+function key(node: Node) {
+  return `${node.x},${node.y}:${node.dir}`
+}
+
+function canGo(from: Node, to: Node) {
+  if (!isNeighbour(from, to)) return false
+  if (from.dir === '<') {
+    return to.dir === '<' || (['^', 'v'].includes(to.dir) && from.x === to.x)
   }
-  if (reindeer.direction === '<' || reindeer.direction === '>') {
-    turns.push('^', 'v')
+  if (from.dir === '>') {
+    return to.dir === '>' || (['^', 'v'].includes(to.dir) && from.x === to.x)
   }
-  if (reindeer.direction === '>') {
-    return [
-      { reindeer: { x: reindeer.x + 1, y: reindeer.y, direction: reindeer.direction }, addedScore: 1 },
-      ...turns.map((t) => ({ reindeer: { x: reindeer.x, y: reindeer.y, direction: t }, addedScore: 1000 })),
-    ]
+  if (from.dir === '^') {
+    return to.dir === '^' || (['<', '>'].includes(to.dir) && from.y === to.y)
   }
-  if (reindeer.direction === '<') {
-    return [
-      { reindeer: { x: reindeer.x - 1, y: reindeer.y, direction: reindeer.direction }, addedScore: 1 },
-      ...turns.map((t) => ({ reindeer: { x: reindeer.x, y: reindeer.y, direction: t }, addedScore: 1000 })),
-    ]
-  }
-  if (reindeer.direction === '^') {
-    return [
-      { reindeer: { x: reindeer.x, y: reindeer.y - 1, direction: reindeer.direction }, addedScore: 1 },
-      ...turns.map((t) => ({ reindeer: { x: reindeer.x, y: reindeer.y, direction: t }, addedScore: 1000 })),
-    ]
-  }
-  if (reindeer.direction === 'v') {
-    return [
-      { reindeer: { x: reindeer.x, y: reindeer.y + 1, direction: reindeer.direction }, addedScore: 1 },
-      ...turns.map((t) => ({ reindeer: { x: reindeer.x, y: reindeer.y, direction: t }, addedScore: 1000 })),
-    ]
-  }
-  throw new Error('')
+  return to.dir === 'v' || (['<', '>'].includes(to.dir) && from.y === to.y)
 }
